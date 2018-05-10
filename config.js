7e35b730c23a3cb967be3a92e24061b2f2358651
Katharine McLeod <katharinemcdevelopment@gmail.com>
2018-05-10 19:53:28 -0400

    creates initial seedPlayer & NPC json files, introduces config file and sets up local Mongoose connection

---
+++ config.js
@@ -0,0 +1,7 @@
+'use strict';
+
+exports.PORT = process.env.PORT || 8080;
+
+exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mkgame';
+
+
--- server.js
+++ server.js
@@ -2,5 +2,32 @@
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const { PORT, MONGODB_URI } = require('./config');
+
 
 app.use(express.static('public'));
+
+// Listen for incoming connections
+if (require.main === module) {
+  mongoose.connect(MONGODB_URI)
+    .then(instance => {
+      const conn = instance.connections[0];
+      console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
+    })
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      console.error('\n === Did you remember to start `mongod`? === \n');
+      console.error(err);
+    });
+  
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
+  
+module.exports = app; // Export for testing
+  
