8d9eca22ad57f9e228a575fc8f180dd654a6e06f
Marina Dargitz <marina.dargitz@gmail.com>
2018-05-20 10:35:39 -0600

    Add NPC post endpoint for testing

--- routes/npcs.js
+++ routes/npcs.js
@@ -12,12 +12,26 @@ router.get('/', (req,res, next)=>{
   Npc.find()
     .then( results => {
       if(results) {
-        return res.json();
+        return res.json(results);
       }
       return next();
-    });
+    })
+    .catch(err => next(err));
 });
 
 //POST a new NPC (not used in app- for testing)
+router.post('/', (req, res, next)=>{
+  const newNpc = {firstName: req.body.firstName,
+    lastName : req.body.lastName};
+
+  Npc.create(newNpc)
+    .then(result => {
+      if (result){
+        return res.status(201).json(result);
+      }
+      return next();
+    })
+    .catch(err => next(err));
+});
 
 module.exports = router;
