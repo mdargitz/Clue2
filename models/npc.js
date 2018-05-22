3b5b93dca4160076e60261915b151fd1eefd4bb3
Katharine McLeod <katharinemcdevelopment@gmail.com>
2018-05-22 15:17:11 -0400

    enforces only one murderer and murderer cnanot die

--- models/npc.js
+++ models/npc.js
@@ -8,23 +8,26 @@ const npcSchema = mongoose.Schema({
   firstName : {type: String, required: true},
   lastName : {type: String, required: true},
   isMurderer : {type: Boolean, default: false },
-  isAlive: {type: Mixed, default: true}
+  isAlive: {type: Boolean, default: true},
 
   // isAlive : {type: Boolean, default: true},
 
   //KRM: technically canDie === !isMurderer. Can probably skip this key
-  // canDie : {type: Boolean, default: true}
+  canDie : {type: Boolean, default: true}
 });
 
 
-//KRM: should force "isMurderer:true" key-value pair to be unique (only one murderer, thanks. need test via postman)
+//Forces "isMurderer:true" key-value pair to be unique
 npcSchema.index({isMurderer: 1}, {unique: true, partialFilterExpression: {isMurderer: true}});
 
 
 //KRM: I *think* this will force the murderer to always be alive. The downside is that this could lead to a turn in which there is no new victim. need test via postman
-npcSchema.pre('save', function () {
+npcSchema.post('update', function () {
+  console.log('hi dere', this instanceof mongoose.Query); 
   if(this.isMurderer === true){
     return this.isAlive === true;
+  }else{
+    return this.preKey === 'present';
   }
 });
 
--- routes/npcs.js
+++ routes/npcs.js
@@ -2,14 +2,88 @@
 
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 
 const Npc = require('../models/npc');
 
 
 //GET all NPC's
 router.get('/', (req,res, next)=>{
-  res.json({'test':'a thing'});
+  const filter = req.query;
+  Npc.find(filter)
+    .then( results => {
+      if(results) {
+        return res.json(results);
+      }
+      return next();
+    })
+    .catch(err => next(err));
 });
+
+//PUT a new NPC used for testing
+// router.put('/:id', (req,res, next)=>{
+// const {id} = req.params;
+//   const updateableFields = ['isAlive', 'canDie','firstName', 'isMurderer'];
+//   const updatedNpc = {};
+//   updateableFields.map(field => {
+//     if (field in req.body){
+//       updatedNpc[field] = req.body[field];
+//     }
+//   });
+//   Npc.findByIdAndUpdate(id, updatedNpc)
+//     .then(result => res.json(result))
+//     .catch(err => next(err));
+
+// });
+
+
+//POST a new NPC (not used in app- for testing)
+router.post('/', (req, res, next)=>{
+  const newNpc = {firstName: req.body.firstName,
+    lastName : req.body.lastName};
+
+  Npc.create(newNpc)
+    .then(result => {
+      if (result){
+        return res.status(201).json(result);
+      }
+      return next();
+    })
+    .catch(err => next(err));
+});
+
+//PUT update a random NPC
+router.put('/random', (req, res, next)=>{
+  const updateableFields = ['isAlive', 'isMurderer'];
+  const updatedNpc = {};
+  updateableFields.map(field => {
+    if (field in req.body){
+      updatedNpc[field] = req.body[field];
+    }
+  });
+
+
+  let query;
+  if('isAlive' in updatedNpc){
+    query = {isMurderer: false};
+  }
+
+
+  console.log(query);
+  Npc.find(query)
+    .then(results => {
+      console.log(results);
+      return Math.floor(Math.random() * results.length);
+    })
+    .then(random => {
+      return Npc.find().limit(1).skip(random);
+    })
+    .then(result => {
+      return Npc.findOneAndUpdate({id : result.id}, updatedNpc, {new : true});
+    })
+    .then(result => res.json(result))
+    .catch(err => next(err));
+
+});
+
 
 module.exports = router;
