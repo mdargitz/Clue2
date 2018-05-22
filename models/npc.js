d5ed2bd36ff236b242e02680e3c95a6e1fa3073d
Katharine McLeod <katharinemcdevelopment@gmail.com>
2018-05-22 15:24:12 -0400

    removing dead code

--- models/npc.js
+++ models/npc.js
@@ -9,28 +9,11 @@ const npcSchema = mongoose.Schema({
   lastName : {type: String, required: true},
   isMurderer : {type: Boolean, default: false },
   isAlive: {type: Boolean, default: true},
-
-  // isAlive : {type: Boolean, default: true},
-
-  //KRM: technically canDie === !isMurderer. Can probably skip this key
-  canDie : {type: Boolean, default: true}
 });
 
 
 //Forces "isMurderer:true" key-value pair to be unique
 npcSchema.index({isMurderer: 1}, {unique: true, partialFilterExpression: {isMurderer: true}});
-
-
-//KRM: I *think* this will force the murderer to always be alive. The downside is that this could lead to a turn in which there is no new victim. need test via postman
-npcSchema.post('update', function () {
-  console.log('hi dere', this instanceof mongoose.Query); 
-  if(this.isMurderer === true){
-    return this.isAlive === true;
-  }else{
-    return this.preKey === 'present';
-  }
-});
-
 
 //Prettify database returns
 npcSchema.set('toObject', {
